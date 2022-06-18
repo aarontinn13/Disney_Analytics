f6d54de3286703bdfecf993ca9199f7aa6eb5524
Aaron Tinn <35347521+aarontinn13@users.noreply.github.com>
2022-06-17 23:58:04 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 import os
-import math
 
 # set options
 pd.set_option('display.max_columns', None)
@@ -17,35 +17,51 @@ def test_path():
             print(os.path.join(dirname, filename))
 
 # print the sample of the data
-print(data_set.sample(5))
+# print(data_set.sample(5))
 
 def describe_data():
-    # print(data_set.describe(include='all'))
-    print(data_set.sample(5))
+    print(data_set.describe(include='all'))
+    #print(data_set.sample(5))
 
-def median():
+def quick_stats():
 
-    values = []
+    values = OrderedDict()
 
     for index, row in data_set.iterrows():
 
         total_sqft = row['TotalBsmtSF'] + row['GrLivArea']
 
-        if row['Neighborhood'] in values:
-            pass
-        else:
-            values += row['Neighborhood']
-
-        if row['YrSold'] == 2006 and total_sqft >= total_sqft:
+        if row['YrSold'] == 2006 and total_sqft >= 2000:
             # print(index, row['Neighborhood'], row['YrSold'], row['TotalBsmtSF'] + row['GrLivArea'], row['SalePrice'])
-
+            if row['Neighborhood'] not in values.keys():
+                # add key and create list
+                values[row['Neighborhood']] = []
+            values[row['Neighborhood']].append(row['SalePrice'])
         else:
             pass
 
-math
+    for key, value in values.items():
+
+        s = pd.Series(value)
+        print(f'{key}:')
+        print(f'{s.describe()}')
+
+def seasonality():
+
+    values = OrderedDict()
+
+    for index, row in data_set.iterrows():
+        if (row['MoSold'], row['YrSold']) not in values.keys():
+            values[(row['MoSold'], row['YrSold'])] = 1
+        else:
+            values[(row['MoSold'], row['YrSold'])] += 1
+
+    for key, value in values.items():
+        print(key, value)
 
 
 if __name__ == '__main__':
-    iterate()
+    #quick_stats()
+    seasonality()
 
 
